d7154c1f69a8cceaf5e72c95150e57fb2023f427
BovineBeta <bovinefbik@foxmail.com>
2025-07-15 16:24:01 +0800

    feat: music sync (download)

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { DB } from "./db.ts";
 
 if (import.meta.main) {
   while (true) {
-    console.log("Commands: add / add not dova / view / exit");
+    console.log("Commands: add / add not dova(WIP) / view(WIP) / sync / exit");
     const input: string | null = prompt(">");
     switch (input) {
       case null:
@@ -12,6 +12,9 @@ if (import.meta.main) {
         break;
       case "add":
         add();
+        break;
+      case "sync":
+        await DB.sync();
         break;
       case "add not dova":
       case "view":
@@ -29,17 +32,24 @@ function add() {
   if (!input) {
     console.log("no input anything");
     return;
-  } else {
-    const [name, author] = input.split(" composed by ");
-    const input2 = prompt("music id>");
-    if (!input2) {
-      console.log("no input anything");
-      return;
-    } else {
-      const id: string = input2;
-      console.log(`Add music: ${name} - ${author} ${DB.getUrl(id)}`);
-      DB.add(author, name, id);
-      return;
-    }
   }
+
+  const [name, author] = input.split(" composed by ");
+  const input2 = prompt("music id>");
+  if (!input2) {
+    console.log("no input anything");
+    return;
+  }
+
+  const input3 = prompt("how many tracks>");
+  if (!input3) {
+    console.log("no input anything. default: only 1 track");
+  }
+  const tracks = input3 ? Number(input3) : 1;
+  const id: string = input2;
+  console.log(
+    `Add music: ${name} - ${author} ${tracks} tracks ${DB.getDovaUrl(id)}`,
+  );
+  DB.addDova(author, name, id, tracks);
+  return;
 }
