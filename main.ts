9ebbfcff7495153e96c336d144e269b1ba79a00e
BovineBeta <bovinefbik@foxmail.com>
2025-07-15 14:03:50 +0800

    feat: add music

---
+++ main.ts
@@ -0,0 +1,45 @@
+import { DB } from "./db.ts";
+
+if (import.meta.main) {
+  while (true) {
+    console.log("Commands: add / add not dova / view / exit");
+    const input: string | null = prompt(">");
+    switch (input) {
+      case null:
+      case "exit":
+        console.log("Program exit.");
+        Deno.exit();
+        break;
+      case "add":
+        add();
+        break;
+      case "add not dova":
+      case "view":
+        console.log("WIP");
+        break;
+      default:
+        console.log("Unknow command.");
+        break;
+    }
+  }
+}
+function add() {
+  console.log("Add dova music:");
+  const input = prompt("[name] composed by [Author]>");
+  if (!input) {
+    console.log("no input anything");
+    return;
+  } else {
+    const [name, author] = input.split(" composed by ");
+    const input2 = prompt("music id>");
+    if (!input2) {
+      console.log("no input anything");
+      return;
+    } else {
+      const id: string = input2;
+      console.log(`Add music: ${name} - ${author} ${DB.getUrl(id)}`);
+      DB.add(author, name, id);
+      return;
+    }
+  }
+}
