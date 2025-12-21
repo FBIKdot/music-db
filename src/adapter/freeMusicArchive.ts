8763b827ff68236ce193fc52aaa037332b9af344
FBIKdot <FBIKdot@users.noreply.github.com>
2025-12-21 23:57:24 +0800

    refactor(adapter/fma): 初始化简写

--- src/adapter/freeMusicArchive.ts
+++ src/adapter/freeMusicArchive.ts
@@ -4,12 +4,11 @@ import { DefaultAdapter, DefaultStyle } from "./default.ts";
 export class FreeMusicArchiveAdapter extends DefaultAdapter {
   private constructor() {
     super();
-    this.DB = Database.get<DefaultStyle[]>(this.PATH);
   }
 
   public override readonly PATH: string = "freemusicarchive";
 
-  protected override readonly DB: DefaultStyle[];
+  protected override readonly DB = Database.get<DefaultStyle[]>(this.PATH);
 
   public static override Instance: FreeMusicArchiveAdapter;
   static {
